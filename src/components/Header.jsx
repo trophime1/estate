d0684b6a941eb262a27fe3433d6ce8a2ba40878d
trophime1 <trophime250@gmail.com>
2023-11-26 21:41:01 +0200

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import{FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-slate-500'>View</span>
+        <span className='text-slate-700'>Home</span>
+
+    </h1>
+    </Link>
+    <form className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-64 '>
+        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none' />
+        <FaSearch className="text-slate-600 " />
+    </form>
+    <ul className="flex gap-4 cursor-pointer">
+        <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <Link to='/'>
+            Home
+            </Link>
+        </li>
+        <li  className='hidden sm:inline text-slate-700 hover:underline'>
+            <Link to='/about'>
+            About
+            </Link>
+        </li>
+        <li className=' text-slate-700 hover:underline'>
+            <Link to='/sign-in'>
+            SignIn
+            </Link>
+        </li>
+    </ul>
+    
+        </div>
+    
+    </header>
+  )
+}
