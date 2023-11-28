7eef575a2ff8008b61cac676bc847fe3d3abd1b1
trophime1 <trophime250@gmail.com>
2023-11-28 22:55:02 +0200

    sign up ai

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center front-semibold my-7'>Sign up</h1>
+    <form className='flex flex-col gap-4'>
+      <input type="text" placeholder="UserName" className='border p-3 rounded-lg' id='username' />
+      <input type="email" placeholder="email" className='border p-3 rounded-lg' id='email' />
+      <input type="text" placeholder="password" className='border p-3 rounded-lg' id='password' />
+      <button type='submit' className='bg-blue-500 hover:bg-blue-600 transition duration-20 
+      p-3 rounded-lg uppercase '>Sign up</button>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>have any account?</p>
+      <Link to='/sign-in'>
+        <span className='cursor-pointer text-blue-700 '> Sign In</span>
+      </Link>
+    </div>
+    </div>
   )
 }
