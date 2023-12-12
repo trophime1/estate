63afea4da62d0faccad06325fc72283219538784
trophime1 <trophime250@gmail.com>
2023-12-12 22:29:56 +0200

    google sign in

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         },
         avatar: {
             type: String,
-            default: "https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg"
+            default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
         },
 
 }, {timestamps : true}
