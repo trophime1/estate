486c01abea3761caa67567c34d519734cf709e2f
trophime1 <trophime250@gmail.com>
2023-12-13 20:24:26 +0200

    update Api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from "../utils.js/error.js";
+import { errorHandler } from "../utils/error.js";
 import Jwt  from "jsonwebtoken";
 export const signup= async (req,res,next)=>{
     const {username,email,password} = req.body;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+
 
 const route = express.Router();
  route.get('/test', test)
+ route.post('/update/:id',verifyToken, updateUser)
  export default route;
