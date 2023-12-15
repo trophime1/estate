82dc7b6b66c9aa04e009638c832686b5bec4eea3
trophime1 <trophime250@gmail.com>
2023-12-15 20:53:41 +0200

    creating listing api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { google, signOut, signin, signup } from "../controllers/auth.controller.js";
 
-const route = express.Router();
-route.post('/signup',signup);
-route.post('/signin',signin);
-route.post('/google',google);
-route.get('/signout', signOut);
-export default route;
+const router = express.Router();
+router.post('/signup',signup);
+router.post('/signin',signin);
+router.post('/google',google);
+router.get('/signout', signOut);
+export default router;
 
 
 
