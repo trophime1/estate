8e6867041b6b5903486f4d25d234fc99a85192eb
trophime1 <trophime250@gmail.com>
2023-12-19 19:02:08 +0200

    signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { signinStart, signinFail ,signinSuccess } from '../redux/user/userSlice'
-import OAuth from '../components/Oauth'
+import OAuth from '../components/OAuth'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
