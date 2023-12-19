54c27f36ab9c684fdf520483e5207209a8ee019f
trophime1 <trophime250@gmail.com>
2023-12-19 18:48:46 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then (()=>{
    console.log(err)
 })
 const app =express();
+const _dirname =path.resolve()
 app.use(express.json())
 app.use(cookieParser());
  app.listen(3000, ()=> {
@@ -22,6 +24,11 @@ app.use(cookieParser());
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
+app.use(express.static(path.join(_dirname, '/client/dist')))
+//handling any other routes that are not found in the above middlewares
+app.get('*', (req,res)=> {
+   res.sendFile(path.join(_dirname ,"client","dist","index.html"))
+   });
 
 
 app.use((err,req,res,next)=>{
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,77 +1,77 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
-import { useSelector } from 'react-redux';
 import {
-    FaBath,
-    FaBed,
-    FaChair,
-    FaMapMarkedAlt,
-    FaMapMarkerAlt,
-    FaParking,
-    FaShare,
-  } from 'react-icons/fa';
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
 import Contact from '../components/Contact';
 
-
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
-
-    const params = useParams();
-    const [listing, setListing] = useState(null);
+  const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
   const [contact, setContact] = useState(false);
+  const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
 
-    useEffect(() => {
-        const fetchListing = async () => {
-          try {
-            setLoading(true);
-            const res = await fetch(`/api/listing/get/${params.listingId}`);
-            const data = await res.json();
-            if (data.success === false) {
-              setError(true);
-              setLoading(false);
-              return;
-            }
-            setListing(data);
-            setLoading(false);
-            setError(false);
-          } catch (error) {
-            setError(true);
-            setLoading(false);
-          }
-        };
-        fetchListing();
-      }, [params.listingId]);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
   return (
     <main>
-       {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
         <p className='text-center my-7 text-2xl'>Something went wrong!</p>
       )}
       {listing && !loading && !error && (
         <div>
-        <Swiper navigation>
-          {listing.imageUrls.map((url) => (
-            <SwiperSlide key={url}>
-              <div
-                className='h-[550px]'
-                style={{
-                  background: `url(${url}) center no-repeat`,
-                  backgroundSize: 'cover',
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className='h-[550px]'
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
             <FaShare
               className='text-slate-500'
               onClick={() => {
@@ -146,10 +146,8 @@ export default function Listing() {
             )}
             {contact && <Contact listing={listing} />}
           </div>
-        
-      </div>
+        </div>
       )}
-    
     </main>
-  )
+  );
 }
