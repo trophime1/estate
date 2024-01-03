6d573f55a1c9503e73b1b1a8bdc87372e0932f16
trophime1 <trophime250@gmail.com>
2024-01-03 18:13:23 +0200

    some changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -51,9 +51,9 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+        Unlock your  <span className='text-slate-500'>dream address</span>
           <br />
-          place to live with ViewHome 
+           with ViewHome 
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
         Your property, your control, with ViewHome.
--- client/src/pages/Recovery.jsx
+++ client/src/pages/Recovery.jsx
@@ -1,6 +1,14 @@
 
 export default function Recovery() {
   return (
-    <div>Recovery</div>
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center front-semibold my-7'>Reset a Password</h1>
+  <form  className='flex flex-col gap-4'>
+    <input type="email" placeholder="New Password" className='border p-3 rounded-lg' id='email'  required />
+    <input type="email" placeholder="Confirm Password" className='border p-3 rounded-lg' id='email'  required />
+    <button className='bg-blue-700 p-3 rounded-lg uppercase text-white '>Verify Email</button>
+    
+  </form>
+  </div>
   )
 }
